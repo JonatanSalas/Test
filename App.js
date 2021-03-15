50a98c0a111bf9fae7d82d021c13d79cf417e6f3
MartinUgarte <39602138+MartinUgarte@users.noreply.github.com>
2021-03-15 15:44:36 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,61 @@
+import Env from 'react-native-config';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity, Alert } from 'react-native';
+
+import MercadoPagoCheckout from '@blackbox-vision/react-native-mercadopago-px';
+
+import styles from './styles';
+
+// You should create the preference server-side, not client-side but we show client-side for the sake of simplicity
+const getPreferenceId = async (payer, ...items) => {
+  const response = await fetch(
+    `https://api.mercadopago.com/checkout/preferences?access_token=${Env.MP_ACCESS_TOKEN}`,
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        items,
+        payer: {
+          email: payer,
+        },
+      }),
+    }
+  );
+
+  const preference = await response.json();
+
+  return preference.id;
+};
+
+export default function App() {
+  const [paymentResult, setPaymentResult] = useState(null);
+
+  const startCheckout = async () => {
+    try {
+      const preferenceId = await getPreferenceId('test_user_8658116@testuser.com', {
+        title: 'Dummy Item Title',
+        description: 'Dummy Item Description',
+        quantity: 1,
+        currency_id: 'ARS',
+        unit_price: 10.0,
+      });
+
+      const payment = await MercadoPagoCheckout.createPayment({
+        publicKey: Env.MP_PUBLIC_KEY,
+        preferenceId,
+      });
+
+      setPaymentResult(payment);
+    } catch (err) {
+      Alert.alert('Something went wrong', err.message);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={startCheckout}>
+        <Text style={styles.text}>Start Payment</Text>
+      </TouchableOpacity>
+      <Text style={styles.text}>Payment: {JSON.stringify(paymentResult)}</Text>
+    </View>
+  );
+}
